40de7bb62b0bf9d946f0849e71b9d7ae83800fe7
Shash <146242897+elyashium@users.noreply.github.com>
2024-09-01 01:30:19 +0530

    Add files via upload

---
+++ country.js
@@ -0,0 +1,80 @@
+const countryName = new URLSearchParams(location.search).get("name");
+const flagImg = document.querySelector('.country-details img');
+const countryNaam = document.querySelector(".country-txt > h1");
+
+const nativeName = document.querySelector('.native-name')
+const population = document.querySelector('.population')
+const region = document.querySelector('.region')
+const subRegion = document.querySelector('.sub-region')
+const capital = document.querySelector('.capital')
+const topLevelDomain = document.querySelector('.top-level-domain')
+const currencies = document.querySelector('.currencies')
+const languages = document.querySelector('.languages')
+const borderCountries = document.querySelector('.border-Country')
+
+const moon = document.querySelector('.moon')
+
+fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`).then((res) => res.json())
+    .then(([country]) => {
+        console.log(country);
+
+        flagImg.src = country.flags.svg;
+        countryNaam.innerText = country.name.common;
+        population.innerText = country.population.toLocaleString('en-IN')
+        region.innerText = country.region
+        topLevelDomain.innerText = country.tld.join(', ')
+
+
+
+        if (country.capital) {
+            capital.innerText = country.capital?.[0]
+        }
+
+        if (country.subregion) {
+            subRegion.innerText = country.subregion
+        }
+
+        if (country.name.nativeName) {
+            nativeName.innerText = Object.values(country.name.nativeName)[0].common
+        } else {
+            nativeName.innerText = country.name.common
+        }
+
+        if (country.currencies) {
+            currencies.innerText = Object.values(country.currencies)
+                .map((currency) => currency.name)
+                .join(', ')
+        }
+
+        if (country.languages) {
+            languages.innerText = Object.values(country.languages).join(', ')
+        }
+
+        if (country.borders) {
+            country.borders.forEach((border) => {
+                fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+                    .then((res) => res.json())
+                    .then(([borderCountry]) => {
+                        // console.log(borderCountry)
+
+                        const countryBorderTab = document.createElement('a');
+                        countryBorderTab.innerText = borderCountry.name.common
+                        countryBorderTab.href = `country.html?name=${borderCountry.name.common}`
+                        borderCountries.append(countryBorderTab);
+
+
+                    });
+            });
+
+        }
+
+        moon.addEventListener('click', () => {
+            document.body.classList.toggle('dark')
+            if (document.body.classList.contains('dark')) {
+                moon.innerText = 'Light Mode';
+            } else {
+                moon.innerText = 'Dark Mode';
+            }
+        })
+
+    });
---
+++ script.js
@@ -0,0 +1,86 @@
+
+const container = document.querySelector(".container");
+const filterByRegion = document.querySelector('.filter-by-region');
+const searchInput = document.querySelector('.search-container input')
+const moon = document.querySelector('.moon')
+
+let allCountriesData;
+
+fetch('https://restcountries.com/v3.1/all')
+    .then((res) => res.json())
+    .then((data) => {
+        renderCountries(data)
+        allCountriesData = data
+    })
+
+filterByRegion.addEventListener('change', (e) => {
+    fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
+        .then((res) => res.json())
+        .then(renderCountries)
+})
+
+
+function renderCountries(data) {
+
+
+    container.innerHTML = ""
+
+    data.forEach(country => {
+
+
+        const cards = document.createElement("a")
+        cards.classList.add("cards");
+        cards.href = `/country.html?name=${country.name.common}`
+
+        //adding the countries page. ${} gives us the name of the counrty card which is clicked.
+
+        // const cardImg = document.createElement("img");
+        // cardImg.src = "https://flagcdn.com/is.svg";
+        // cards.append(cardImg); //adding the img into the card after seperately making them.
+
+        // this is too lengthy we will follow a different apporach.
+
+        const cardHtml = `
+
+            <img src="${country.flags.svg}" alt="flag ${country.name.common}">
+
+            <div class="card-txt">
+            <h3>${country.name.common}</h3>
+            <p><b>Population: </b>${country.population.toLocaleString()}</p>
+            <p><b>Region: </b>${country.region}</p>
+            <p><b>Capital: </b>${country.capital?.[0]}</p>
+            </div>
+
+`
+
+        //?. = optional chaining, will only execute when its present.
+
+        cards.innerHTML = cardHtml;
+        container.append(cards);
+
+    });
+
+
+}
+
+
+
+searchInput.addEventListener('input', (e) => {
+    const filteredCountries = allCountriesData.filter((country) => country.name.common.toLowerCase().includes(e.target.value.toLowerCase()))
+    renderCountries(filteredCountries)
+})
+
+moon.addEventListener('click', () => {
+    document.body.classList.toggle('dark')
+    if (document.body.classList.contains('dark')) {
+        moon.innerText = 'Light Mode';
+    } else {
+        moon.innerText = 'Dark Mode';
+    }
+})
+
+
+
+
+
+
